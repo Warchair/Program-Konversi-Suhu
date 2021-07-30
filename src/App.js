80e8f9f6af6375ef5d8f115ebcc9b3fa28ea0ba1
Warchair <chairulsp2@gmail.com>
2021-07-30 16:44:44 +0700

    configuration for github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Konversi from './components/Konversi';
 import kimia from'./img/kimia.png';
+
+
+function Main() {
+
+  return (
+    <main className="mb-5">
+      <div className="container mb-5"> 
+        <h1 className="pt-5 mb-4 text-center" >Konversi Suhu</h1>
+        <div className="row">
+          <div className="col-md-6 cols-sm-12">
+            <img src={kimia} alt="" className="img-fluid" />
+          </div>
+          <div className="col-md-6 col-sm-12">
+          <Konversi/>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
 
 class App extends React.Component {
 
   render() {
     return (
-        <main className="mb-5">
-          <div className="container mb-5"> 
-            <h1 className="pt-5 mb-4 text-center" >Konversi Suhu</h1>
-            <div className="row">
-              <div className="col-md-6 cols-sm-12">
-                <img src={kimia} alt="" className="img-fluid" />
-              </div>
-              <div className="col-md-6 col-sm-12">
-                <Konversi/>
-              </div>
-            </div>
-          </div>
-        </main>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/Program-Konversi-Suhu" component={Main} />
+          <Redirect from="*" to="/Program-Konversi-Suhu" />
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
